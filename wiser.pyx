564e3c609ff0b878cbac686686ae01e1a6c1927d
Ike Tohru <rokujyouhitomajp@gmail.com>
2015-10-27 22:19:56 +0900

    Care of MemoryError

--- wiser.pyx
+++ wiser.pyx
@@ -6,6 +6,8 @@ from wiser cimport main as c_main
 def main(py_args=[]):
     cdef char **c_argv
     c_argv = <char**>malloc(sizeof(char*) * len(py_args))
+    if not c_argv:
+        raise MemoryError()
     py_args = [b'wiser'] + [bytes(x) for x in py_args]
     for i, s in enumerate(py_args):
         c_argv[i] = s
