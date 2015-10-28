a8fd58cd898e0644ef7364e6eaaf6acf518ac618
Ike Tohru <rokujyouhitomajp@gmail.com>
2015-10-28 23:08:36 +0900

    Add c compiler flags

--- setup.py
+++ setup.py
@@ -23,12 +23,14 @@ libraries = [
     "m",
 ]
 
+CFLAGS="-Wall -std=c99 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -O3 -g"
+
 ext = Extension("wiser",
                 sources=["wiser.pyx"] + sources,
                 include_dirs=include_dirs,
                 libraries=libraries,
                 language="c",
-                extra_compile_args=["-Wall -std=c99 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -O3 -g"])
+                extra_compile_args=[CFLAGS])
 
 setup(name="Python wrapper for wiser by used of Cython",
       ext_modules=cythonize([ext]))
