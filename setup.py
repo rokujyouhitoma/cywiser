5cb418e6867dd4703fa2732c77bedba9a689d119
Ike Tohru <rokujyouhitomajp@gmail.com>
2015-10-30 00:10:01 +0900

    Update gcc options

--- setup.py
+++ setup.py
@@ -23,18 +23,18 @@ libraries = [
     "m",
 ]
 
-DEBUG = False
+DEBUG = True
 if DEBUG:
-    CFLAGS="-Wall -std=c99 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -O3 -g"
+    CFLAGS=["-Wall", "-std=c99", "-D_GNU_SOURCE", "-D_FILE_OFFSET_BITS=64", "-O3", "-g"]
 else:
-    CFLAGS="-Wall -std=c99 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -O3"
+    CFLAGS=["-Wall", "-std=c99", "-D_GNU_SOURCE", "-D_FILE_OFFSET_BITS=64", "-O3", "-g"]
 
 ext = Extension("wiser",
                 sources=["wiser.pyx"] + sources,
                 include_dirs=include_dirs,
                 libraries=libraries,
                 language="c",
-                extra_compile_args=[CFLAGS])
+                extra_compile_args=CFLAGS)
 
 setup(name="Python wrapper for wiser by used of Cython",
       ext_modules=cythonize([ext]))
